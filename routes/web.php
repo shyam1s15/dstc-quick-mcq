05c29c0497d8c33ade118c842201a419982b647e
shyam <shyam1ss15.com>
2020-04-26 12:53:34 +0530

    attached mdb

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('testExample', function () {
     return view('testExample');
+});
+
+
+Route::get('/', function($id) {
+    return view('index.welcome');
 });
