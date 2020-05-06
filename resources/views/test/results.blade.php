409616b4ec1c9893338c8a132269ccc0c8ad58ff
shyam <shyam1ss15.com>
2020-05-07 00:00:31 +0530

    lkknj

--- resources/views/test/results.blade.php
+++ resources/views/test/results.blade.php
@@ -1,125 +1,144 @@
-
 <h2 class="mb-1 mt-1 font-weight-bold text-center green-text">The Results are now out congratulations </h2>
 
 @foreach ($levels as $index => $level)
 
 @if ( $marks[$index] < $level->Elite )
-    
-{{--  passing section  --}}
-<div class="row justify-content-md-center">
-  <div class="col-4 col-lg-3">
-    <div class="symmentric-padding-2 mr-2 ml-2">
-      <form class="p-1 grey-text">
-        <div class="md-form form-sm">
-          {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
-          {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
-          <h5 class="required">{{ $level->branch_subject }}
-            <i class="fa fa-arrow-right red-text ml-2" aria-hidden="true"></i>
-          </h5>
 
+    {{--  passing section  --}}
+    <div class="row justify-content-md-center">
+        <div class="col-4 col-lg-3">
+            <div class="symmentric-padding-2 mr-2 ml-2">
+                <form class="p-1 grey-text">
+                    <div class="md-form form-sm">
+                        {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
+                        {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
+                        <h5 class="required">{{ $level->branch_subject }}
+                            <i class="fa fa-arrow-right red-text ml-2" aria-hidden="true"></i>
+                        </h5>
+
+                    </div>
+                </form>
+            </div>
         </div>
-      </form>
-    </div>
-  </div>
 
-  <div class="col-4 col-lg-3">
-    <div class="symmentric-padding-2 mr-2 ml-2">
-      <form class="p-1 grey-text">
-        <div class="md-form form-sm">
-          {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
-          {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
+        <div class="col-4 col-lg-3">
+            <div class="symmentric-padding-2 mr-2 ml-2">
+                <form class="p-1 grey-text">
+                    <div class="md-form form-sm">
+                        {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
+                        {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
 
-          <div class="md-form form-sm">
-            <i class="fab fa-angellist prefix"></i>
-            {{--  <input type="number" name="passing-marks" id="passingMarks" class="form-control form-control-sm" required>  --}}
-            <label for="passing" class="required" id="passingLabel">{{ $marks[ $index ] }} out of {{ $level->passing }}</label>
-          </div>
+                        <div class="md-form form-sm">
+                            <i class="fab fa-angellist prefix"></i>
+                            {{--  <input type="number" name="passing-marks" id="passingMarks" class="form-control form-control-sm" required>  --}}
+                            <label for="passing" class="required" id="passingLabel">{{ $marks[ $index ] }} out of
+                                {{ $level->passing }}</label>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-      </form>
     </div>
-  </div>
-</div>
 
 
-<div class="row justify-content-md-center">
+    <div class="row justify-content-md-center">
 
-  <div class="col-md-12 col-lg-6">
-    <div class="symmentric-padding-2 mr-2 ml-2">
-      <form class="p-1 grey-text">
-        <div class="md-form form-sm">
-          {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
-          {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
-          <div class="md-form form-sm"><i class="fas fa-envelope prefix green-text"></i>
-            <input type="text" name="passing-marks" id="passingMsg" class="form-control form-control-sm" disabled>
-            <label for="passingMsg" class="required"> {{ $level->passing_msg }} </label>
-          </div>
+        <div class="col-md-12 col-lg-6">
+            <div class="symmentric-padding-2 mr-2 ml-2">
+                <form class="p-1 grey-text">
+                    <div class="md-form form-sm">
+                        {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
+                        {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
+                        <div class="md-form form-sm"><i class="fas fa-envelope prefix green-text"></i>
+                            <input type="text" name="passing-marks" id="passingMsg" class="form-control form-control-sm"
+                                disabled>
+                            <label for="passingMsg" class="required"> {{ $level->passing_msg }} </label>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-      </form>
     </div>
-  </div>
-</div>
 
-{{--  over passing section  --}}
+    {{--  over passing section  --}}
 
-@else
-    
-{{--  Elite section  --}}
-<div class="row justify-content-md-center">
-  <div class="col-4 col-lg-3">
-    <div class="symmentric-padding-2 mr-2 ml-2">
-      <form class="p-1 grey-text">
-        <div class="md-form form-sm">
-          {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
-          {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
-          <h5 class="required">{{ $level->branch_subject }}
-            <i class="fa fa-arrow-right red-text ml-3" aria-hidden="true"></i>
-          </h5>
+    @else
 
+    {{--  Elite section  --}}
+    <div class="row justify-content-md-center">
+        <div class="col-4 col-lg-3">
+            <div class="symmentric-padding-2 mr-2 ml-2">
+                <form class="p-1 grey-text">
+                    <div class="md-form form-sm">
+                        {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
+                        {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
+                        <h5 class="required">{{ $level->branch_subject }}
+                            <i class="fa fa-arrow-right red-text ml-3" aria-hidden="true"></i>
+                        </h5>
+
+                    </div>
+                </form>
+            </div>
         </div>
-      </form>
-    </div>
-  </div>
 
-  <div class="col-4 col-lg-3">
-    <div class="symmentric-padding-2 mr-2 ml-2">
-      <form class="p-1 grey-text">
-        <div class="md-form form-sm">
-          {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
-          {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
+        <div class="col-4 col-lg-3">
+            <div class="symmentric-padding-2 mr-2 ml-2">
+                <form class="p-1 grey-text">
+                    <div class="md-form form-sm">
+                        {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
+                        {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
 
-          <div class="md-form form-sm">
-            <i class="fab fa-angellist prefix"></i>
-            {{--  <input type="number" name="Elite-marks" id="EliteMarks" class="form-control form-control-sm" required>  --}}
-            <label for="EliteMarks" class="required" id="EliteMarksLabel">{{ $marks[ $index ] }} out of {{ $level->Elite }}</label>
-          </div>
+                        <div class="md-form form-sm">
+                            <i class="fab fa-angellist prefix"></i>
+                            {{--  <input type="number" name="Elite-marks" id="EliteMarks" class="form-control form-control-sm" required>  --}}
+                            <label for="EliteMarks" class="required" id="EliteMarksLabel">{{ $marks[ $index ] }} out of
+                                {{ $level->Elite }}</label>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-      </form>
     </div>
-  </div>
-</div>
 
 
-<div class="row justify-content-md-center">
+    <div class="row justify-content-md-center">
 
-  <div class="col-md-12 col-lg-6">
-    <div class="symmentric-padding-2 mr-2 ml-2">
-      <form class="p-1 grey-text">
-        <div class="md-form form-sm">
-          {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
-          {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
-          <div class="md-form form-sm"><i class="fas fa-envelope prefix green-text"></i>
-            <input type="text" name="Elite-marks" id="EliteMsg" class="form-control form-control-sm" disabled>
-            <label for="EliteMsg" class="required">{{ $level->Elite_msg }}</label>
-          </div>
+        <div class="col-md-12 col-lg-6">
+            <div class="symmentric-padding-2 mr-2 ml-2">
+                <form class="p-1 grey-text">
+                    <div class="md-form form-sm">
+                        {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
+                        {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
+                        <div class="md-form form-sm"><i class="fas fa-envelope prefix green-text"></i>
+                            <input type="text" name="Elite-marks" id="EliteMsg" class="form-control form-control-sm"
+                                disabled>
+                            <label for="EliteMsg" class="required">{{ $level->Elite_msg }}</label>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-      </form>
     </div>
-  </div>
-</div>
 
-{{--  over Elite section  --}}
-@endif
+    {{--  over Elite section  --}}
+    @endif
+    @endforeach
+    <div class="row justify-content-md-center">
+        <div class="col-4 col-lg-3">
+            <div class="symmentric-padding-2 mr-2 ml-2">
+                <form class="p-1 grey-text">
+                    <div class="md-form form-sm">
+                        {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
+                        {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
 
-<a href="{{ env('APP_URL') }}/student/showChoices" class="btn btn-danger"> Finish </a>
-@endforeach
-
+                        <div class="md-form form-sm">
+                            <a href="{{ env('APP_URL') }}/student/showChoices" class="btn btn-danger">
+                                <i class="fas fa-allergies fa-lg"></i>    
+                                Finish
+                             </a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
