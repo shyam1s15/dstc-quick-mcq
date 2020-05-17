0ebc014a78784154046e1ead90981e4547bc9937
shyam1s15 <shyam1ss15@gmail.com>
2020-05-17 08:47:47 +0530

    oops

--- resources/views/test/resultPreview.blade.php
+++ resources/views/test/resultPreview.blade.php
@@ -1,3 +1,7 @@
+@extends('base.header_onlyFooterSocialMedia')
+
+@section('main-content')
+
 <h2 class="mb-1 mt-1 font-weight-bold cust-dstc-nav-color text-center">The Results are now Out.</h2>
 @if ( $higgest_subjects )
 <h4 class="mb-1 mt-1 font-weight-bolder cust-dstc-nav-color text-center">Most Suitable branch for you :
@@ -6,4 +10,6 @@
     @endforeach
 </h4>
 @endif
+
+@endsection
 
