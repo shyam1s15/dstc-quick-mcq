12c6570f2e0f054f9b65eea2e03f3f909c22d8ec
shyam1s15 <shyam1ss15@gmail.com>
2020-05-17 10:09:37 +0530

    card layout added to results

--- resources/views/test/resultPreview.blade.php
+++ resources/views/test/resultPreview.blade.php
@@ -3,13 +3,23 @@
 @section('main-content')
 
 <h2 class="mb-1 mt-1 font-weight-bold cust-dstc-nav-color text-center">The Results are now Out.</h2>
+
 @if ( $higgest_subjects )
+
+<div class="row">
+    <div class="col-12 col-lg-8">
 <h4 class="mb-1 mt-1 font-weight-bolder cust-dstc-nav-color text-center">Most Suitable branch for you :
     @foreach ($higgest_subjects as $subject)
-        {{ $subject->branch_subject }} <br>
-    @endforeach
-</h4>
-@endif
+    <div class="card">
 
+    {{ $subject->branch_subject }} <br>
+    </div>
+    @endforeach
+
+</h4>
+    </div>
+</div>
+
+@endif
 @endsection
 
