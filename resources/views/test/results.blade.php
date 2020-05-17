add2ff6a4b046e6a8ab350862028e1bffb5b0d7e
shyam1s15 <shyam1ss15@gmail.com>
2020-05-17 10:41:23 +0530

    some extras in results

--- resources/views/test/results.blade.php
+++ resources/views/test/results.blade.php
@@ -1,10 +1,22 @@
+
 <h2 class="mb-1 mt-1 font-weight-bold cust-dstc-nav-color text-center">The Results are now Out.</h2>
+
 @if ( $higgest_subjects )
-<h4 class="mb-1 mt-1 font-weight-bolder cust-dstc-nav-color text-center">Most Suitable branch for you :
+
+<div class="row justify-content-center">
+    <div class="col-12 col-lg-6">
+<h4 class="mb-1 mt-1 font-weight-bold green-text text-center">Most Suitable branch for you :
     @foreach ($higgest_subjects as $subject)
-        {{ $subject->branch_subject }} <br>
+    <div class="card mt-2 font-weight-bolder green-text">
+
+    {{ $subject->branch_subject }} <br>
+    </div>
     @endforeach
+
 </h4>
+    </div>
+</div>
+
 @endif
 
 
