e12458a041f92fa90b49a8cd5cbc8c9737c07bd7
shyam1s15 <shyam1ss15@gmail.com>
2020-05-17 13:32:52 +0530

    as requirement said button given

--- resources/views/quiz/completed.blade.php
+++ resources/views/quiz/completed.blade.php
@@ -3,13 +3,18 @@
 
 @section('main-content')
 
-<h1> thankyou for showing intrest in our university </h1>
+<h2 class="mb-1 mt-1 font-weight-bold cust-dstc-nav-color text-center">Thank you for attempting.</h2>
 
+<div class="row justify-content-center">
+    <div class="col-8 col-lg-6 text-center">
+        <button class="btn btn-primary" id="start_again">Start Again</button>
+    </div>
+</div>
 <script>
 
-    $(document).ready(function(){
-        var delay = 15000;
-        var url = "https://www.google.com";
+    $("#start_again").click(function(){
+        var delay = 0;
+        var url = "{{ env('APP_URL') }}/student/showChoices";
         setTimeout(function(){ window.location = url; }, delay);
     });
 </script>
