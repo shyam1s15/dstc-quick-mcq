d5e2033a8acd1585ed23ec00d464727d489c9f3e
shyam1s15 <shyam1ss15@gmail.com>
2020-05-18 11:07:19 +0530

    wlskafm

--- resources/views/test/app/mcqs.blade.php
+++ resources/views/test/app/mcqs.blade.php
@@ -45,7 +45,7 @@
                                     @if (!empty($question->option1) > 0)
 
                                     <!--Grid column-->
-                                    <div class="col-md-4 col-lg-5">
+                                    <div class="col-md-4 col-lg-5  ml-1 mr-1 p-0">
                                         <div class="symmentric-padding-2 mr-2 ml-2">
                                             <form class="pr-1 pl-1 grey-text form-check">
                                                 <div class="md-form form-sm"> <i
@@ -71,7 +71,7 @@
 
                                     <!--Grid column-->
 
-                                    <div class="col-md-4 col-lg-5">
+                                    <div class="col-md-4 col-lg-5  ml-1 mr-1 p-0">
                                         <div class="symmentric-padding-2 mr-2 ml-2">
                                             <form class="pr-1 pl-1 grey-text">
                                                 <div class="md-form form-sm"> <i
@@ -102,7 +102,7 @@
                                 <div class="row justify-content-md-center">
 
                                     <!--Grid column-->
-                                    <div class="col-md-4 col-lg-5">
+                                    <div class="col-md-4 col-lg-5  ml-1 mr-1 p-0">
                                         <div class="symmentric-padding-2 mr-2 ml-2">
                                             <form class="pr-1 pl-1 grey-text">
                                                 <div class="md-form form-sm"> <i
@@ -128,7 +128,7 @@
                                     @if (!empty($question->option4) > 0)
 
                                     <!--Grid column-->
-                                    <div class="col-md-4 col-lg-5">
+                                    <div class="col-md-4 col-lg-5  ml-1 mr-1 p-0">
                                         <div class="symmentric-padding-2 mr-2 ml-2">
                                             <form class="pr-1 pl-1 grey-text">
                                                 <div class="md-form form-sm"> <i
