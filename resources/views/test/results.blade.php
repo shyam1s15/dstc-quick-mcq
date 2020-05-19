dba4b89f1872df221d8fd1dd3c800692ef7ff773
shyam <shyam1ss15.com>
2020-05-19 11:26:20 +0530

    kjfn

--- resources/views/test/results.blade.php
+++ resources/views/test/results.blade.php
@@ -152,50 +152,6 @@
 </div>
 
 
-@else
-<div class="card mt-3" id="randomQuestionForm">
-
-    <div class="row justify-content-md-center">
-
-        <div class="col-6 col-lg-3">
-
-            <div class="symmentric-padding-2 mr-2 ml-2">
-                <form class="grey-text">
-                    <div class="md-form form-sm">
-                        {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
-                        {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
-                        <h5 class="grey-text font-weight-bolder">{{ $level->branch_subject }}
-
-                        </h5>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-        <div class="col-6 col-lg-3">
-            <div class="symmentric-padding-2 mr-2 ml-2">
-                <form class="grey-text">
-                    <div class="md-form form-sm">
-                        {{--  <input type="text" id="form3" class="form-control form-control-sm" required>  --}}
-                        {{--  <input type="file" style="display: none" id="questionImageFile">  --}}
-
-                        <div class="md-form form-sm">
-                            <i class="fab fa-angellist prefix"></i>
-                            {{--  <input type="number" name="passing-marks" id="passingMarks" class="form-control form-control-sm" required>  --}}
-                            <label for="passing" class="grey-text font-weight-bolder"
-                                id="passingLabel">{{ $marks[ $index ] }} out of
-                                {{ count($level->questions) }}</label>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
-
-    {{--  over passing section  --}}
-</div>
-
 @endif
 
 @endforeach
