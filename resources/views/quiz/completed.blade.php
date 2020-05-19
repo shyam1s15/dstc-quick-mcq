d4bbf3c05f65f2060db9c4cf03ef5113c6192f21
shyam <shyam1ss15.com>
2020-05-19 09:23:33 +0530

    kdjsfksdj

--- resources/views/quiz/completed.blade.php
+++ resources/views/quiz/completed.blade.php
@@ -2,16 +2,20 @@
 
 
 @section('main-content')
+<div class="d-flex align-items-center justify-content-center" style="height: 350px">
 
-<h2 class="mb-1 mt-1 font-weight-bold cust-dstc-nav-color text-center">Thank you for attempting.</h2>
 
-<div class="row justify-content-center">
-    <div class="col-8 col-lg-6 text-center">
-        <button class="btn btn-primary" id="start_again">Start Again</button>
+
+    <div class="row justify-content-center">
+        <div class="col-8 col-lg-6 text-center">
+            <h2 class="mb-1 mt-1 font-weight-bold cust-dstc-nav-color text-center">Thank you for attempting.</h2>
+        </div>
+    <button class="btn btn-primary" id="start_again">Start Again</button>
+
     </div>
+
 </div>
 <script>
-
     $("#start_again").click(function(){
         var delay = 0;
         var url = "{{ env('APP_URL') }}/student/showChoices";
