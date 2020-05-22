6829eab4fee51a4bfd1db30ac1b451101fbc01c2
shyam <shyam1ss15.com>
2020-05-22 18:02:30 +0530

    try this

--- app/Http/Controllers/makeExcelResultsController.php
+++ app/Http/Controllers/makeExcelResultsController.php
@@ -4,19 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\appFormDataModel;
-use Maatwebsite\Excel\Excel;
+use Maatwebsite\Excel\Facades\Excel;
 use App\exports\easyResultsExports;
 
 class makeExcelResultsController extends Controller
 {
     //
-    public function create_results(Request $request){
+    public function create_results(Request $request)
+    {
         $app_id = $request->input('app_id');
         $app = appFormDataModel::find($app_id);
-        // return $app->tempStudentResults;
-        // return Excel::create();
-        // $excel = new Excel();
+        // // return $app->tempStudentResults;
+        // // return Excel::create();
+        // // $excel = new Excel();
         return Excel::download(new easyResultsExports, 'invoices.xlsx');
-
     }
 }
