139c1c33f77a710262f8772dd2c959f155d807e4
shyam <shyam1ss15.com>
2020-05-22 18:07:56 +0530

    jugad bhai jugad

--- app/Http/Controllers/makeExcelResultsController.php
+++ app/Http/Controllers/makeExcelResultsController.php
@@ -10,13 +10,14 @@ use App\exports\easyResultsExports;
 class makeExcelResultsController extends Controller
 {
     //
+    
     public function create_results(Request $request)
     {
         $app_id = $request->input('app_id');
         $app = appFormDataModel::find($app_id);
-        // // return $app->tempStudentResults;
+        // return $app->tempStudentResults;
         // // return Excel::create();
         // // $excel = new Excel();
-        return Excel::download(new easyResultsExports, 'invoices.xlsx');
+        return Excel::download(new easyResultsExports($app_id), 'jugad.xlsx');
     }
 }
--- app/exports/easyResultsExports.php
+++ app/exports/easyResultsExports.php
@@ -4,12 +4,16 @@ namespace App\exports;
 
 use App\new_student;
 use Maatwebsite\Excel\Concerns\FromCollection;
-
+use App\appFormDataModel;
 class easyResultsExports implements FromCollection
 {
     //
+    public function __construct( $app_id)
+    {
+        $this->app_id = $app_id;
+    }
     public function collection()
     {
-        return new_student::all();
+        return appFormDataModel::find($this->app_id);
     }
 }
